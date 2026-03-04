e56e8e0c506fc5570f98c846cfa71038bc150f87
teffidev <tefyberal09@gmail.com>
2026-03-04 08:41:34 -0500

    Add Abstract Class and Circle Class

---
+++ HeritageWorkshop/HeritageWorkshop.Backend/Circle.cs
@@ -0,0 +1,34 @@
+﻿namespace HeritageWorkshop.Backend;
+
+public class Circle : GeometricFigure
+{
+    //Fields
+    private double _r;
+
+    //Constructors
+    public Circle(string name, double r) : base(name)
+    {
+        R = r;
+    }
+
+    //Properties
+    public double R 
+    { 
+        get => _r; 
+        set => _r = ValidateR(value); 
+    }
+
+    //Methods
+    public override double GetArea() => Math.PI * Math.Pow(R, 2);
+
+    public override double GetPerimeter() => 2 * Math.PI * R;
+
+    public override string ToString() => base.ToString();
+
+    private double ValidateR(double r)
+    {
+        if (r <= 1)
+            throw new ArgumentException("Radius must be greater than zero.");
+        return r;
+    }
+}
---
+++ HeritageWorkshop/HeritageWorkshop.Backend/GeometricFigure.cs
@@ -0,0 +1,24 @@
+﻿namespace HeritageWorkshop.Backend;
+
+public abstract class GeometricFigure
+{
+    //Constructors
+    public GeometricFigure(string name)
+    {
+        Name = name;
+    }
+
+    //Properties
+    public string Name { get; set; } = null!;
+
+    //Methods
+    override public string ToString()
+    {
+        return $"{Name, -15} => Area.....: {GetArea(), 15:N5}\tPerimeter: {GetPerimeter(), 15:N5}";
+    }
+
+    public abstract double GetArea();
+
+    public abstract double GetPerimeter();
+
+}
