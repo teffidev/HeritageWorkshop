c8b09bcef27ddab67d7d40725599a933898b583f
teffidev <tefyberal09@gmail.com>
2026-03-05 16:49:00 -0500

    Add Rectangle Class

---
+++ HeritageWorkshop/HeritageWorkshop.Backend/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿namespace HeritageWorkshop.Backend;
+
+public class Rectangle : Square
+{
+    //Fields
+    public double _b;
+
+    //Constructors
+    public Rectangle(string name, double a, double b) : base(name, a)
+    {
+        B = b;
+    }
+
+    //Properties
+    public double B
+    {
+        get => _b;
+        set => _b = ValidateB(value);
+    }
+
+    //Methods
+    public override double GetArea() => A * B;
+    public override double GetPerimeter() => 2 * (A + B);
+
+    private double ValidateB(double b)
+    {
+        if (b <= 0)
+            throw new ArgumentException("Side B must be greater than zero.");
+        return b;
+    }
+}
--- HeritageWorkshop/HeritageWorkshop.Frontend/Program.cs
+++ HeritageWorkshop/HeritageWorkshop.Frontend/Program.cs
@@ -4,10 +4,11 @@ var circle = new Circle(name: nameof(Circle), r: 5);
 var square = new Square(name: nameof(Square), a: 10);
 var rhombus = new Rhombus(name: nameof(Rhombus), a: 5, d1: 7, d2: 10);
 var kite = new Kite(name: nameof(Kite), a: 7, b: 8, d1: 6, d2: 5);
+var rectangle = new Rectangle(name: nameof(Rectangle), a: 4.568, b: 67.790);
 
 var figures = new List<GeometricFigure>
 {
-    circle, square, rhombus, kite
+    circle, square, rhombus, kite, rectangle
 };
 
 foreach (var figure in figures)
