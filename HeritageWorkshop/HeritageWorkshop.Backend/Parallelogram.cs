7cba2cc3d69077b4c85bf3288c9d437911083ae8
teffidev <tefyberal09@gmail.com>
2026-03-05 16:53:27 -0500

    Add Parallelogram Class

---
+++ HeritageWorkshop/HeritageWorkshop.Backend/Parallelogram.cs
@@ -0,0 +1,31 @@
+﻿namespace HeritageWorkshop.Backend;
+
+public class Parallelogram : Rectangle
+{
+    //Fields
+    private double _h;
+
+    //Constructors
+    public Parallelogram(string name, double a, double b, double h) : base(name, a, b)
+    {
+        H = h;
+    }
+
+    //Properties
+    public double H
+    {
+        get => _h;
+        set => _h = ValidateH(value);
+    }
+
+    //Methods
+    public override double GetArea() => B * H;
+    public override double GetPerimeter() => 2 * (A + B);
+
+    private double ValidateH(double h)
+    {
+        if (h <= 0)
+            throw new ArgumentException("Height must be greater than zero.");
+        return h;
+    }
+}
