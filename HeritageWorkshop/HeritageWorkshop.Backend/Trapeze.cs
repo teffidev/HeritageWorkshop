3ac570b7c50b3f973c934efe7ea6b4b10ef7777b
teffidev <tefyberal09@gmail.com>
2026-03-05 17:07:48 -0500

    Add Trapeze Class

---
+++ HeritageWorkshop/HeritageWorkshop.Backend/Trapeze.cs
@@ -0,0 +1,31 @@
+﻿namespace HeritageWorkshop.Backend;
+
+public class Trapeze : Triangle
+{
+    //Fields
+    private double _d;
+
+    //Constructor
+    public Trapeze(string name, double a, double b, double c, double d, double h) : base(name, a, b, c, h)
+    {
+        D = d;
+    }
+
+    //Properties
+    public double D
+    {
+        get => _d;
+        set => _d = ValidateD(value);
+    }
+
+    //Methods
+    public override double GetArea() => (B + D) * H / 2;
+    public override double GetPerimeter() => A + B + C + D;
+
+    private double ValidateD(double d)
+    {
+        if (d <= 0)
+            throw new ArgumentException("Side D must be greater than zero.");
+        return d;
+    }
+}
