f2a5dd548edf8f3ae27c6c94d7c03d841c21ff8d
teffidev <tefyberal09@gmail.com>
2026-03-05 12:11:01 -0500

    Fix import to frontend program

--- HeritageWorkshop/HeritageWorkshop.Frontend/Program.cs
+++ HeritageWorkshop/HeritageWorkshop.Frontend/Program.cs
@@ -1,3 +1,13 @@
 ﻿using HeritageWorkshop.Backend;
 
-var cicle = new Cicle();
+var circle = new Circle(name: nameof(Circle), r: 5);
+
+var figures = new List<GeometricFigure>
+{
+    circle,
+};
+
+foreach (var figure in figures)
+{
+    Console.WriteLine(figure);
+}
